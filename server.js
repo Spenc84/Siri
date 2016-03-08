dc471459eac6076a2829179442368adab5e6bae1
Spencer Powell <spenc84@gmail.com>
2016-03-08 12:38:36 -0600

    another update

--- server.js
+++ server.js
@@ -9,8 +9,28 @@ var randomizer = function(){
 };
 
 // GET POST PUT
-app.get('/response', function( req, res ){
-  res.send(JSON.stringify({ message: randomizer() }));
+app.options('/', function(req, res) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send();
+});
+
+app.get('/', function( req, res ){
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify({ message: randomizer() }));
 });
 
 app.listen(8887, function(){
